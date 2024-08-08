2017949618a037fed6f5a2cc27f433edca7de919
Anas Iqbal <mohd.abd.6602@gmail.com>
2024-08-08 22:41:16 +0530

    refactor: Update Google Maps integration in VenueAccommodation page

--- src/pages/VenueAccommodation.js
+++ src/pages/VenueAccommodation.js
@@ -71,7 +71,7 @@ function VenueAccommodation() {
                                 </div>
 
                                 <div class="overflow-hidden rounded-lg lg:col-span-2 h-96 lg:h-auto">
-                                    <iframe width="100%" height="100%" frameborder="0" title="map" marginheight="0" marginwidth="0" scrolling="no" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"></iframe>
+                                    <iframe width="100%" height="100%" frameborder="0" title="map" marginheight="0" marginwidth="0" scrolling="no" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=National%20Institute%20of%20Technology,%20Sector%201,%20Rourkela,%20Odisha&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"></iframe>
                                 </div>
                             </div>
                         </div>
