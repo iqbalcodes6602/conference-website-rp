3994ba89bb4bfec8f0a8ce1388f70de26b59e50e
Anas Iqbal <mohd.abd.6602@gmail.com>
2024-09-07 09:00:53 +0530

    added static past events card

--- client/src/sections/home/components/pastevents-card.jsx
+++ client/src/sections/home/components/pastevents-card.jsx
@@ -1,25 +1,59 @@
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid";
 import {
   Card,
   CardBody,
   Typography,
 } from "@material-tailwind/react";
+import { Link } from "react-router-dom";
 
 
-export function PastEventsCard({ title='', description='', subTitle='' }) {
+const events = [
+  {
+    _id: 1,
+    title: 'Event 1',
+    year: '2024',
+    description: ' - This is the first event.',
+    link: '/event1'
+  },
+  {
+    _id: 2,
+    title: 'Event 2',
+    year: '2024',
+    description: ' - This is the second event.',
+    link: '/event2'
+  },
+  {
+    _id: 3,
+    title: 'Event 3',
+    year: '2024',
+    description: ' - This is the third event.',
+    link: '/event3'
+  }
+];
+
+export function PastEventsCard() {
   return (
     <Card shadow={false}>
       <CardBody className="h-full p-5 flex flex-col items-center rounded-2xl bg-gray-900 opacity-0.5 ">
-        <Typography variant="h6" className="mb-4 text-center" color="white">
-          {subTitle}
-        </Typography>
-        <Typography variant="h4" className="text-center" color="white">
-          {title}
+        <Typography variant="h4" className="text-left mb-5" color="white">
+          Latest Announcements
         </Typography>
         <Typography
           color="white"
           className="mt-2 mb-10 text-base w-full lg:w-10/12 text-justify font-normal"
         >
-          {description}
+          <ul>
+            {events.map((event) => (
+              <li key={event._id}>
+                <Typography color="white" className="mb-2 flex items-baseline">
+                <span className='text-orange-500'>{event.title + ' ' + event.year }</span> {event.description}
+                  <Link to={event.link}>
+                    <ArrowTopRightOnSquareIcon className='w-4 h-4 ml-2' />
+                  </Link>
+                </Typography>
+              </li>
+            ))}
+          </ul>
         </Typography>
       </CardBody>
     </Card>
